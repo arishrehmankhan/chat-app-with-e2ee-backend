f000f0a0a197309aec5e4564b0e0de933f9854f9
Arish Rehman Khan <arish.rehman.khan@gmail.com>
2020-12-06 17:52:00 +0530

    initial commit

---
+++ src/main/java/com/arish/chatapp/ChatAppWithE2eeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arish.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppWithE2eeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppWithE2eeBackendApplication.class, args);
+	}
+
+}
