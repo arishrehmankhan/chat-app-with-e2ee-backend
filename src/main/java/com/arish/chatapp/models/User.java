5fd55c2d6b53f117d708568782da36c711614979
GAURAV KUMAR <gaurav.kr109@gmail.com>
2021-01-09 20:46:33 +0530

    File updated

--- src/main/java/com/arish/chatapp/models/User.java
+++ src/main/java/com/arish/chatapp/models/User.java
@@ -13,56 +13,78 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sun.istack.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 
 @Entity
 public class User {
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@JsonProperty
-	private Long id;
-	@NotNull @Column(unique = true)
-	private String username;
-	@NotNull
-	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-	private String password;
-	@NotNull
-	private String firstname;
-	@NotNull
-	private String lastname;
-	@CreationTimestamp
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty
+    private Long id;
+    @NotNull
+    @Column(unique = true)
+    private String username;
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    @NotNull
+    private String firstname;
+    @NotNull
+    private String lastname;
+    @Value("")
+    private String forgotPasswordToken;
+    @CreationTimestamp
     private LocalDateTime createdAt;
     @UpdateTimestamp
     private LocalDateTime updatedAt;
-    
-	public User() {}
-	
-	public User(String username, String password) {
-		this.username = username;
-		this.password = password;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getFirstname() {
-		return firstname;
-	}
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-	public String getLastname() {
-		return lastname;
-	}
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-	
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setForgotPasswordToken(String token) {
+        forgotPasswordToken = token;
+    }
+
+    public String getForgotPasswordToken() {
+        return forgotPasswordToken;
+    }
+
 }
